404f10f539ed68dbedbb831b7156f8891c15d59f
Amir <ghafourzadea@gmail.com>
2023-05-21 10:34:23 +0330

    faet: customizable-date-picker
    some optimization for lower size of production mode

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,2 @@
 import CustomizableDatePicker from "@components/CustomizableDatePicker";
-import DatePicker from "@components/DatePicker";
-import { gregorianCalendar, jalaliCalendar } from "@services/Calendar";
-export { CustomizableDatePicker, DatePicker, gregorianCalendar, jalaliCalendar };
+export { CustomizableDatePicker };
